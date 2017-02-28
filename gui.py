ff6e43d46b18eec372d2a8176248a120e554b65d
Ethan Barker <ethanbarker0@gmail.com>
2017-02-27 17:20:28 -0800

    Changed to login manager, databased managed with numpy

---
+++ gui.py
@@ -0,0 +1,104 @@
+from tkinter import *
+from tkinter import messagebox
+import os
+import numpy as np
+
+
+# Load Database
+database = np.load('database.npy').item()
+# print(database)
+
+# Setup window
+root = Tk()
+root.minsize(width=100, height=30)
+root.title("Login")
+
+# Global variables
+cwd =  os.path.dirname(os.path.realpath(__file__)) # Current working directory
+passwordFound = False
+
+
+class MakeLabel:
+    def __init__(self, type, rowNum, columnNum, input, win):
+        if type == "label":
+            label = Label(win, text=input)
+            label.grid(row = rowNum, column = columnNum)
+        if type == "entry":
+            self.entry = Entry(win)
+            self.entry.grid(row = rowNum, column = columnNum)
+        if type == "button":
+            button = Button(win, text=input, command= lambda: password.login(None))
+            button.grid(column = columnNum)
+        if type == "reg":
+            button = Button(win, text=input, command= lambda: password.register(None))
+            button.grid(column = columnNum)
+        if type == "registerConfirm":
+            button = Button(win, text=input, command= lambda: password.createUser(None))
+            button.grid(column = columnNum)
+
+    def next(self, event):
+        password.entry.focus_set()
+
+    def register(self, event):
+        self.regWin = Toplevel(root)
+        self.regWin.title("Register")
+        MakeLabel("label", 0, 0, "New Username", self.regWin)
+        self.newUsername = MakeLabel("entry", 0, 1, "", self.regWin)
+        MakeLabel("label", 1, 0, "Password", self.regWin)
+        self.newPassword = MakeLabel("entry", 1, 1, "", self.regWin)
+        MakeLabel("label", 2, 0, "Verify Password", self.regWin)
+        self.verifyPassword = MakeLabel("entry", 2, 1, "", self.regWin)
+        MakeLabel("registerConfirm", 2, 0, "Confirm", self.regWin)
+        self.newPassword.entry.config(show="*")
+        self.verifyPassword.entry.config(show="*")
+
+        database[username.entry.get()] = password.entry.get()
+        np.save('database.npy', database)
+
+    def createUser(self, event):
+        if self.newPassword.entry.get() == '':
+            messagebox.showinfo("Error", "Please enter a new password")
+        elif self.newPassword.entry.get() == self.verifyPassword.entry.get():
+            if self.newUsername.entry.get() in database:
+                messagebox.showinfo("Error", "Username already taken")
+            else:
+                database[self.newUsername.entry.get()] = self.newPassword.entry.get()
+                np.save('database.npy', database)
+                messagebox.showinfo("Success", "Registered")
+                self.regWin.destroy()
+        elif self.verifyPassword.entry.get() == '':
+            messagebox.showinfo("Error", "Please verify your password")
+        else:
+            messagebox.showinfo("Error", "Passwords do not match")
+
+
+    def login(self, event):
+        global passwordFound
+        if password.entry.get() == "":
+            messagebox.showinfo("Error", "Please enter a password")
+            return
+        try:
+            if database[username.entry.get()] == password.entry.get():
+                messagebox.showinfo("Success", "Logging in")
+            else:
+                messagebox.showinfo("Error", "Password not found")
+        except KeyError:
+            messagebox.showinfo("Error", "Password not found")
+
+# Use with caution
+def clearDatabase():
+    clear = {}
+    np.save('database.npy', clear)
+
+# Setup Tkinter objects
+MakeLabel("label", 0, 0, "Username", root)
+username = MakeLabel("entry", 0, 1, "", root)
+MakeLabel("label", 1, 0, "Password", root)
+password = MakeLabel("entry", 1, 1, "", root)
+MakeLabel("button", 2, 0, "Confirm", root)
+MakeLabel("reg", 2, 0, "Register", root)
+username.entry.bind("<Return>", username.next)
+password.entry.bind("<Return>", password.login)
+password.entry.config(show="*")
+
+root.mainloop()
